3579e5ca02158b56997ce77604b7ee669e9e220c
“Minggo Zheng <516930389@qq.com>
2019-05-19 17:33:10 +0800

    新增：codec network

--- codec/codec.go
+++ codec/codec.go
@@ -1,1 +1,35 @@
+// 使用自带库gob进行编码/解码
 package codec
+
+import (
+	"bytes"
+	"encoding/gob"
+)
+
+// 客户端与服务端之间传输的数据类型
+type Data struct {
+	Name string        // service名称
+	Args []interface{} // 传参列表
+	Err  string        // error
+}
+
+// Encode
+func Encode(data Data) ([]byte, error) {
+	var buf bytes.Buffer
+	encoder := gob.NewEncoder(&buf)
+	if err := encoder.Encode(data); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+// Decode
+func Decode(b []byte) (Data, error) {
+	buf := bytes.NewBuffer(b)
+	decoder := gob.NewDecoder(buf)
+	var data Data
+	if err := decoder.Decode(&data); err != nil {
+		return Data{}, err
+	}
+	return data, nil
+}
--- network/network.go
+++ network/network.go
@@ -1,1 +1,44 @@
 package network
+
+import (
+	"encoding/binary"
+	"io"
+	"net"
+)
+
+// Transport struct
+type Transport struct {
+	conn net.Conn
+}
+
+// New Transport
+func New(conn net.Conn) *Transport {
+	return &Transport{conn}
+}
+
+// Send data
+// 数据格式：数据长度（4字节） + 数据
+func (this *Transport) Send(data *[]byte) error {
+	buf := make([]byte, 4+len(*data))
+	binary.BigEndian.PutUint32(buf[:4], uint32(len(*data))) // Set Header field
+	copy(buf[4:], *data)                                    // Set Data field
+	_, err := this.conn.Write(buf)
+	return err
+}
+
+// Receive data
+// 先读数据长度（4字节）再读数据
+func (this *Transport) Receive() (*[]byte, error) {
+	header := make([]byte, 4)
+	_, err := io.ReadFull(this.conn, header)
+	if err != nil {
+		return nil, err
+	}
+	dataLen := binary.BigEndian.Uint32(header)
+	data := make([]byte, dataLen)
+	_, err = io.ReadFull(this.conn, data)
+	if err != nil {
+		return nil, err
+	}
+	return &data, err
+}
