230844153571dd0b5a71599818c2d502a96d676f
“Minggo Zheng <516930389@qq.com>
2019-05-19 20:15:59 +0800

    新增：server

--- network/network.go
+++ network/network.go
@@ -8,37 +8,37 @@ import (
 
 // Transport struct
 type Transport struct {
-	conn net.Conn
+	conn *net.Conn
 }
 
 // New Transport
-func New(conn net.Conn) *Transport {
+func New(conn *net.Conn) *Transport {
 	return &Transport{conn}
 }
 
 // Send data
 // 数据格式：数据长度（4字节） + 数据
-func (this *Transport) Send(data *[]byte) error {
+func (t *Transport) Send(data *[]byte) error {
 	buf := make([]byte, 4+len(*data))
 	binary.BigEndian.PutUint32(buf[:4], uint32(len(*data))) // Set Header field
 	copy(buf[4:], *data)                                    // Set Data field
-	_, err := this.conn.Write(buf)
+	_, err := (*t.conn).Write(buf)
 	return err
 }
 
 // Receive data
 // 先读数据长度（4字节）再读数据
-func (this *Transport) Receive() (*[]byte, error) {
+func (t *Transport) Receive() ([]byte, error) {
 	header := make([]byte, 4)
-	_, err := io.ReadFull(this.conn, header)
+	_, err := io.ReadFull(*t.conn, header)
 	if err != nil {
 		return nil, err
 	}
 	dataLen := binary.BigEndian.Uint32(header)
 	data := make([]byte, dataLen)
-	_, err = io.ReadFull(this.conn, data)
+	_, err = io.ReadFull(*t.conn, data)
 	if err != nil {
 		return nil, err
 	}
-	return &data, err
+	return data, err
 }
--- server/server.go
+++ server/server.go
@@ -1,1 +1,108 @@
 package server
+
+import (
+	"Ming-RPC/codec"
+	"Ming-RPC/network"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"reflect"
+)
+
+// TODO 暂时把所有service放在map中，后续接入Etcd实现服务注册与发现
+type Server struct {
+	addr     string
+	services map[string]reflect.Value
+}
+
+// New Server
+func New(addr string) *Server {
+	return &Server{addr: addr, services: make(map[string]reflect.Value)}
+}
+
+// 注册方法
+func (s *Server) Register(name string, f interface{}) {
+	if _, ok := s.services[name]; ok {
+		return
+	}
+	s.services[name] = reflect.ValueOf(f)
+}
+
+// Run
+func (s *Server) Run() {
+	l, err := net.Listen("tcp", s.addr)
+	if err != nil {
+		log.Printf("listen on %s err: %v\n", s.addr, err)
+		return
+	}
+	for {
+		conn, err := l.Accept() //  Accept()方法会阻塞
+		if err != nil {
+			log.Printf("accept err: %v\n", err)
+			continue
+		}
+
+		//  由于goroutine比线程简便得多，而且Golang中的IO底层实现方式和java NIO模型一致，都采用了EPOLL
+		//  所以可以为每个连接都创建一个新的goroutine来处理
+		go func(s *Server, conn net.Conn) {
+			defer conn.Close()
+			socket := network.New(&conn)
+			for {
+				data, err := socket.Receive() // 这里也会阻塞
+				if err != nil {
+					if err != io.EOF {
+						log.Printf("read err: %v\n", err)
+					}
+					return
+				}
+				req, err := codec.Decode(data)
+				checkError(err, "decode err")
+				f, ok := s.services[req.Name]
+				if !ok {
+					e := fmt.Sprintf("func %s does not exist", req.Name)
+					log.Println(e)
+					response := codec.Data{Name: req.Name, Err: e}
+					responseBytes, err := codec.Encode(response)
+					checkError(err, "encode err")
+					if err = socket.Send(&responseBytes); err != nil {
+						log.Printf("transport write err: %v\n", err)
+					}
+					continue
+				}
+				log.Printf("func %s is called\n", req.Name)
+				// 把请求列表（interface{}）转成reflect.Value类型
+				inArgs := make([]reflect.Value, len(req.Args))
+				for i := range req.Args {
+					inArgs[i] = reflect.ValueOf(req.Args[i])
+				}
+				// call 对应 service
+				out := f.Call(inArgs)
+				// 把reflect.Value类型转回interface{}（不包括err，所以len - 1）
+				outArgs := make([]interface{}, len(out)-1)
+				for i := 0; i < len(out)-1; i++ {
+					outArgs[i] = out[i].Interface()
+				}
+				// 处理error
+				var e string
+				if _, ok := out[len(out)-1].Interface().(error); !ok {
+					e = ""
+				} else {
+					e = out[len(out)-1].Interface().(error).Error()
+				}
+				// send response to client
+				response := codec.Data{Name: req.Name, Args: outArgs, Err: e}
+				responseBytes, err := codec.Encode(response)
+				checkError(err, "encode err")
+				err = socket.Send(&responseBytes)
+				checkError(err, "socket write err")
+			}
+		}(s, conn)
+	}
+}
+
+func checkError(err error, logValue string) {
+	if err != nil {
+		log.Printf("%s: %v\n", logValue, err)
+	}
+}
